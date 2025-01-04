cd1b463304cac4fc8ec1dc8783f1f17a5721b09b
ammrbhlwn <muammar.bahalwan@gmail.com>
2025-01-04 20:31:58 +0700

    feat: slicing landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'alpina-grey': '#828FA1',
+        'alpina-light-grey': '#A3B0C2',
+        'alpina-blue': '#362EED',
+        'alpina-orange': '#FF9A3D',
+        'alpina-gren': '#07B704',
+        'alpina-black': '#060521',
       },
     },
   },
